93291b9c46fa6c46035e6d9a6013a1b17e202e3f
franciscoryb1 <franciscoryb1@hotmail.com>
2026-02-02 21:28:19 -0300

    ADD chatbot-movements.controller.ts

--- src/movements/movements.module.ts
+++ src/movements/movements.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MovementsService } from './movements.service';
 import { MovementsController } from './movements.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { ChatbotMovementsController } from './chatbot-movements.controller';
+import { UsersService } from '../users/users.service';
 
 @Module({
-    controllers: [MovementsController],
-    providers: [MovementsService, PrismaService],
+    controllers: [MovementsController, ChatbotMovementsController],
+    providers: [PrismaService, MovementsService, UsersService],
 })
 export class MovementsModule { }
